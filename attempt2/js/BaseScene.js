d70c597354e48b244ecabf51666443e731f1e95e
jmjs125 <61749329+jmjs125@users.noreply.github.com>
2020-03-06 12:55:32 +0000

    Add files via upload
    i have added a hey and got collision  to work for my door

--- attempt2/js/BaseScene.js
+++ attempt2/js/BaseScene.js
@@ -1,37 +1,44 @@
-class BaseScene extends Phaser.Scene{
+class BaseScene extends Phaser.Scene {
     cursors;
-   enemies;
+    enemies;
     player;
-    door
-   
+    door;
+    blocks = [];
+    keys;
+    haskey = false;
 
-    constructor(config, tilesetImageURL, tilemapURL,tilesetItemsURL) {
+
+    constructor(config, tilesetImageURL, tilemapURL, tilesetItemsURL) {
         super(config);
         this.tilesetImageURL = tilesetImageURL;
         this.tilemapURL = tilemapURL;
         this.tilesetItemsURL = tilesetImageURL;
-      
+
     }
 
-    preload() { 
-        
+    preload() {
+
         this.load.image('tileset-image', this.tilesetImageURL);
-       this.load.image('mountains','/assets/backgroundCastles.png');
-       this.load.image("items",this.tilesetItemsURL);
-       this.load.image('tilesets')
-       this.load.image('doors','/assets/door.png');    
-        
+        this.load.image('mountains', '/assets/backgroundCastles.png');
+        this.load.image("items", this.tilesetItemsURL);
+        this.load.image('tilesets', this.tilesetItemsURL);
+        this.load.image('doors', '/assets/door.png');
+        this.load.image('bblocker', 'assets/block_exported.png');
+        this.load.image('yblocker', 'assets/ylock_exported.png');
+        this.load.image('gblocker', 'assets/glock_exported.png');
+        this.load.image('keys', '/assets/greenkey.png');
+
         this.load.spritesheet('player', 'assets/wizard.png', {
             frameWidth: 32,
             frameHeight: 32
         });
-        this.load.spritesheet('enemy','assets/enemyblue.png',{
-            frameHeight:32,
+        this.load.spritesheet('enemy', 'assets/enemyblue.png', {
+            frameHeight: 32,
             frameWidth: 32
         });
-        
 
-        
+
+
         this.load.tilemapTiledJSON('level', this.tilemapURL);
     }
 
@@ -39,60 +46,76 @@ class BaseScene extends Phaser.Scene{
         this.map = this.make.tilemap({
             key: 'level'
         });
+        this.keys = this.physics.add.staticGroup();
         this.enemies = this.physics.add.group();
         this.doors = this.physics.add.staticGroup();
-       
+        //this.block = this.physics.add.staticGroup();
+
+
         this.map.tileset = this.map.addTilesetImage('floor', 'tileset-image');
 
         this.physics.world.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
 
         this.map.createStaticLayer('background', this.map.tileset, 0, 0);
         this.map.createStaticLayer('floor', this.map.tileset, 0, 0);
-        
-       //this.createplayer();
+
+        //this.createplayer();
 
         let objectLayer = this.map.getObjectLayer("objects");
         if (objectLayer) {
             objectLayer.objects.forEach(function (object) {
                 object = this.retrieveCustomProperties(object); //Check if the object has any custom properties in Tiled and assign them to the object
-               // console.log(object)
+                // console.log(object)
 
                 if (object.type === "spawnpoint") {
                     //Create player
                     this.createPlayer(object);
-                }else if(object.type === "enemyspawner"){
+                } else if (object.type === "enemyspawner") {
                     this.createEnemy(object);
-                }else if(object.type === "blockers"){
-                    this.createblockers(objects);
-                
-                }else if(object.type === "door"){
+                    console.log('this is working');
+                } else if (object.type === "blocker") {
+
+
+                    this.blocks.push(object);
+
+
+
+
+                } else if (object.type === "door") {
+
                     this.createdoors(object);
-        
-                    
-               
-                }else if (object.type === "key"){
+
+
+
+                } else if (object.type === "keys") {
+                    this.createkey(object);
+                    console.log(this.keys);
 
                 }
             }, this); //Set context for object layer
-           
-        } 
-       this.mountains = this.add.image(this.map.widthInPixels, this.map.heightInPixels,'mountains');
-       //console.log(this.mountains);
-       this.mountains.setScale(10,10);
-       this.mountains.setDepth(-1);
-       
-        
+
+        }
+        for (var i = 0; i < this.blocks.length; i++) {
+            this.createblockers(this.blocks[i]);
+
+        }
+        this.mountains = this.add.image(this.map.widthInPixels, this.map.heightInPixels, 'mountains');
+        //console.log(this.mountains);
+        this.mountains.setScale(10, 10);
+        this.mountains.setDepth(-1);
+
+
 
         this.camera = this.cameras.getCamera("");
         this.camera.startFollow(this.player);
         this.camera.setBounds(0, 0, this.map.widthInPixels, this.map.height * this.map.tileHeight);
         this.camera.zoom = 1;
-           
+
         this.createCollision();
         this.cursors = this.input.keyboard.createCursorKeys()
 
-    
-    
+
+
         this.anims.create({
             key: 'Left',
             frames: this.anims.generateFrameNumbers('player', {
@@ -124,11 +147,12 @@ class BaseScene extends Phaser.Scene{
             frameRate: 10,
             repeat: -1
         })
-     
-        
+       
+      
+
 
     }
- 
+
     update() {
         //console.log(this.player);
         if (this.cursors.right.isDown) {
@@ -148,21 +172,38 @@ class BaseScene extends Phaser.Scene{
         if (Phaser.Input.Keyboard.JustDown(this.cursors.space)) {
             this.player.setVelocityY(-500);
         }
-        
+
 
     }
-    createdoors(object){
-        
-        this.doors.create(object.x,object.y,'doors');
-        console.log(this.doors);
-        
+    createdoors(object) {
+
+        this.doors.create(object.x, object.y, 'doors');
+        //console.log(this.doors);
+
 
     }
-   createblockers(object){
-       this.blockers.create(object.x/2,object.y/2,sprite.object);
-   }
+    createblockers(object) {
 
-    
+        let name = object.name;
+
+
+        let block = this.add.sprite(object.x, object.y, name);
+        this.physics.add.existing(block);
+        block.body.allowGravity = false;
+        block.body.setImmovable(true);
+        //this.physics.add.existing(block);
+        //console.log(this.blocks);
+
+
+        this.physics.add.collider(this.player, block, this.colliderblock, null, this);
+
+
+    }
+    createkey(object) {
+        this.keys.create(object.x, object.y, 'keys');
+    }
+
+
 
     retrieveCustomProperties(object) {
         if (object.properties) { //Check if the object has custom properties
@@ -183,33 +224,55 @@ class BaseScene extends Phaser.Scene{
     }
     createPlayer(object) {
         this.player = this.physics.add.sprite(object.x, object.y, 'player', 0);
-       this.player.setCollideWorldBounds(true);
-       this.player.setScale(1.5)
-       this.player.setDepth(2)
+        this.player.setCollideWorldBounds(true);
+        this.player.setScale(1.5)
+        this.player.setDepth(2)
     }
-   
 
-    createCollision(){
-       // this is making a name and making = layer from tiled named 'floor' 
-       //and tilesetImageURL = my ground.png image that is used to make floor
+
+    createCollision() {
+        // this is making a name and making = layer from tiled named 'floor' 
+        //and tilesetImageURL = my ground.png image that is used to make floor
         let collisionLayer = this.map.getLayer('floor').tilemapLayer;
-       
-        collisionLayer.setCollisionBetween(0,1000)
-        
-        
-        this.physics.add.collider(this.player, collisionLayer);
-        this.physics.add.collider(this.enemies,collisionLayer);
-        this.physics.add.collider(this.doors,this.player,this.collidedoor,null,this);
-        this.physics.add.collider(this.enemies,this.player,this.enemyattack,null,this);
-    }
 
-    collidedoor(){
-        console.log("this has coillided ")
+        collisionLayer.setCollisionBetween(0, 1000)
+
+
+        this.physics.add.collider(this.player, collisionLayer);
+        this.physics.add.collider(this.enemies, collisionLayer);
+        this.physics.add.collider(this.enemies, this.player, this.enemyattack, null, this);
+        this.physics.add.collider(this.player,this.keys,this.collidekey,null,this);
+
+
     }
-    enemyattack(){
+    colliderblock() {
+if(this.haskey == false ){
+          console.log("door is lock ")
+      }else if(this.haskey == true){
+          console.log("player has key ")
+      }
+         
+
+
+
+    }
+    collidedoor() {
+      
+      
+
+    }
+    collidekey(){
+      this.haskey = true;
+      this.colliderblock();
+
+        
+
+        
+    }
+    enemyattack() {
         console.log("enemy hit you ");
     }
-    createEnemy(object){
+    createEnemy(object) {
         // //this.enemy = this.physics.add.sprite(object.x,object.y,'enemy',0);
         // this.enemy.setCollideWorldBounds(true);
         // this.enemy.setDepth(2)
@@ -226,8 +289,8 @@ class BaseScene extends Phaser.Scene{
         let enemy = this.add.follower(line, origin.x, origin.y, 'enemy');
         this.physics.add.existing(enemy);
         this.enemies.add(enemy);
-  
-     
+
+
 
         enemy.startFollow({
             duration: 5000,
